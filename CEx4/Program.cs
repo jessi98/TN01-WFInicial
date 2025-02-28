f12da39fcf80f2fed9fc59e8b3877655bb24ceb3
JESSICA BATISTA DOS SANTOS <jessica.bsantos22@senacsp.edu.br>
2025-02-28 19:32:36 -0300

    feat: Exercicio 4 Corrigido no WF

--- CEx4/Program.cs
+++ CEx4/Program.cs
@@ -12,7 +12,7 @@
             int num3 = Convert.ToInt32(Console.ReadLine());
 
             int maior = Math.Max(num1, num2);
-            maior = Math.Max(maior ,num3);
+            maior = Math.Max(maior, num3);
 
             Console.WriteLine($"\nO número {maior} é o maior valor!");
         }

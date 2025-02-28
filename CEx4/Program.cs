480f83340710820c65d55b484b0dcb1f191d9d3e
JESSICA BATISTA DOS SANTOS <jessica.bsantos22@senacsp.edu.br>
2025-02-28 19:19:51 -0300

    feat: Exercicio 4 Corrigido no Console

---
+++ CEx4/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace CEx4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o primeiro número: ");
+            int num1 = int.Parse(Console.ReadLine()!);
+            Console.Write("\nDigite o segundo número: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\nDigite o segundo número: ");
+            int num3 = Convert.ToInt32(Console.ReadLine());
+
+            int maior = Math.Max(num1, num2);
+            maior = Math.Max(maior ,num3);
+
+            Console.WriteLine($"\nO número {maior} é o maior valor!");
+        }
+    }
+}

21f210c3df2acdb746e12007d98580ef3b56cb2a
JESSICA BATISTA DOS SANTOS <jessica.bsantos22@senacsp.edu.br>
2025-02-27 22:21:59 -0300

    feat: Exercicio 3 Corrigido no Console

---
+++ CEx3/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace CEx3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o primeiro número: ");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.Write("\nDigite o segundo número: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            int maior = Math.Max(num1, num2);
+
+            Console.WriteLine($"\nO número {maior} é o maior valor!");
+        }
+    }
+}
